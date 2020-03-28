ebb126085b75169e581cf311b5cac77a482a365c
Julian George <jules104@gmail.com>
2020-03-28 11:10:41 +0000

    All tests passing

--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -7,6 +7,11 @@ describe '#capitalize' do
     it "Returns ['AbC', 'aBc'] when input is 'abc'" do
         expect(capitalize("abc")).to eq ['AbC', 'aBc']
     end
-
+    it "Returns ['AbCdEf', 'aBcDeF'] when input is 'abcdef'" do
+        expect(capitalize("abcdef")).to eq ['AbCdEf', 'aBcDeF']
+    end
+    it "Returns ['AbRaCaDaBrA', 'aBrAcAdAbRa'] when input is 'abracadabra'" do
+        expect(capitalize("abracadabra")).to eq ['AbRaCaDaBrA', 'aBrAcAdAbRa']
+    end
 end
 
